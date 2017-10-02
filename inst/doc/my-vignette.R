b875be499fcf5248f5e7804be6fd354855358393
Simone <simone@simones-mac-pro.homenet.telecomitalia.it>
2017-10-02 10:16:14 +0200

    add vignette, and figure

--- inst/doc/my-vignette.R
+++ inst/doc/my-vignette.R
@@ -1,7 +1,13 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ---- eval=FALSE---------------------------------------------------------
+#  source("https://bioconductor.org/biocLite.R")
+#  biocLite("GMQL")
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ---- eval=FALSE---------------------------------------------------------
+#  test_url = "http://130.186.13.219/gmql-rest"
+#  login.GMQL(test_url)
+#  downloadDataset(test_url,"dataset_test",path = getwd())
+
+## ---- eval=FALSE---------------------------------------------------------
+#  test_url = "http://130.186.13.219/gmql-rest"
+#  login.GMQL(test_url)
 
