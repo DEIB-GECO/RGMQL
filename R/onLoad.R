c16932d86227aa8b35e0eeb3252fba06f03f55dd
David B. Dahl <david@ddahl.org>
2017-10-02 14:39:12 -0600

    Set "major.version" in .rscalaPackage function.

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  .rscalaPackage(pkgname, heap.maximum = "4g",serialize.output=TRUE)
+  .rscalaPackage(pkgname, heap.maximum = "4g",serialize.output=TRUE, major.release=c("2.11"))
   ## Assign 'WrappeR' to package environment before it is sealed, but don't fulfill promise until needed.
   assign("WrappeR",s$.it.polimi.genomics.r.Wrapper,envir=parent.env(environment()))
 
